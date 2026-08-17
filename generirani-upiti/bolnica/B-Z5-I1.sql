b071893440c7b0ab63221cac7c7dec780d72dfb2
Marina Matešić <marinamatesic959@gmail.com>
2026-08-17 12:34:24 +0200

    Create B-Z5-I1.sql

---
+++ generirani-upiti/bolnica/B-Z5-I1.sql
@@ -0,0 +1,38 @@
+WITH broj_operacija AS (
+SELECT
+YEAR(o.DATUM) AS godina,
+MONTH(o.DATUM) AS mjesec,
+o.ID_SALE,
+COUNT(*) AS broj_operacija
+FROM OPERACIJA AS o
+GROUP BY
+YEAR(o.DATUM),
+MONTH(o.DATUM),
+o.ID_SALE
+),
+rangirano AS (
+SELECT
+godina,
+mjesec,
+ID_SALE,
+broj_operacija,
+DENSE_RANK() OVER (
+PARTITION BY godina, mjesec
+ORDER BY broj_operacija DESC
+) AS rang
+FROM broj_operacija
+)
+SELECT
+r.godina,
+r.mjesec,
+r.ID_SALE,
+s.TIP_SALE,
+r.broj_operacija
+FROM rangirano AS r
+JOIN SALA AS s
+ON s.ID_SALE = r.ID_SALE
+WHERE r.rang = 1
+ORDER BY
+r.godina,
+r.mjesec,
+r.ID_SALE;
